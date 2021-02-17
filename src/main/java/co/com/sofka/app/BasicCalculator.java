3f734cb516d054121c87572b0abdf86d5e0c46c7
Santiago Mejia <santiagomejiad19@hotmail.com>
2021-02-17 11:12:21 -0500

    Project Configuration

---
+++ src/main/java/co/com/sofka/app/BasicCalculator.java
@@ -0,0 +1,32 @@
+package co.com.sofka.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasicCalculator {
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+
+    public Long sum(Long number1, Long number2) {
+        logger.info( "Summing {} + {}", number1, number2 );
+        return number1 + number2;
+    }
+
+    public Long res(Long number1, Long number2) {
+        logger.info( "Summing {} - {}", number1, number2 );
+        return number1 + number2;
+    }
+
+    public Long mul(Long number1, Long number2) {
+        logger.info( "Summing {} * {}", number1, number2 );
+        return number1 * number2;
+    }
+
+    public Long div(Long number1, Long number2) {
+        logger.info( "Summing {} / {}", number1, number2 );
+        return number1 / number2;
+    }
+
+
+
+}
